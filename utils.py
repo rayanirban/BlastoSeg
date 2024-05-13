a271f0bbd9ec464058ef910ffaa0a5b0b111bee8
Anniek Stokkermans (ext) <anniek.stokkermans@lin-dl4mia-1.htechnopole.local>
2024-05-13 14:06:11 +0200

    add utils.py with a function to create semantic segmentation from  labels

---
+++ utils.py
@@ -0,0 +1,13 @@
+from skimage.segmentation import find_boundaries
+import numpy as np
+
+def instance_to_semantic(labels: np.array) -> np.array: 
+    """Convert instance segmentation to semantic segmentation with a class for background (0), cells (1) and boundaries (2)"""
+    
+    boundaries = find_boundaries(labels, mode = 'inner') > 0
+
+    semantic_seg = np.zeros_like(labels)
+    semantic_seg[labels > 0] = 1
+    semantic_seg[boundaries] = 2
+
+    return semantic_seg
