e958966b1599d49c151b6d5b2649e19e303c0cfb
Anniek Stokkermans <anniek.stokkermans@gmail.com>
2024-05-14 12:20:50 +0200

    implement labels to semantic segmentation in the dataset class

--- utils.py
+++ utils.py
@@ -1,18 +1,14 @@
 from skimage.segmentation import find_boundaries
 import numpy as np
 
-
-def compute_mean() -> np.float32:
-
-    mean = np.mean()
-
 def instance_to_semantic(labels: np.array) -> np.array: 
     """Convert instance segmentation to semantic segmentation with a class for background (0), cells (1) and boundaries (2)"""
     
     boundaries = find_boundaries(labels, mode = 'inner') > 0
 
-    semantic_seg = np.zeros_like(labels)
-    semantic_seg[labels > 0] = 1
-    semantic_seg[boundaries] = 2
+    semantic_seg = np.zeros_like(labels) # background will stay at label 0
+    semantic_seg[labels > 1] = 2 # foreground has label 2
+    semantic_seg[boundaries] = 3 # boundaries have label 3 
+    semantic_seg[labels == 1] = 1 # 1 is the label to be ignored
 
     return semantic_seg
