4f1166a407e44a442ae80850c2b1c92124891c0c
Bart Steemans (ext) <bart.steemans@lin-dl4mia-20.htechnopole.local>
2024-05-16 12:02:03 +0200

    implementation of boundary pixel classification

--- utils.py
+++ utils.py
@@ -3,12 +3,18 @@ import numpy as np
 
 def instance_to_semantic(labels: np.array) -> np.array: 
     """Convert instance segmentation to semantic segmentation with a class for background (0), cells (1) and boundaries (2)"""
-    
-    boundaries = find_boundaries(labels, mode = 'inner') > 0
 
-    semantic_seg = np.zeros_like(labels) # background will stay at label 0
-    semantic_seg[labels > 1] = 2 # foreground has label 2
-    semantic_seg[boundaries] = 3 # boundaries have label 3 
-    semantic_seg[labels == 1] = 1 # 1 is the label to be ignored
+    semantic_seg_stack = np.zeros_like(labels) # 118,256,256
 
-    return semantic_seg
+    for i in range(0, labels.shape[0]):
+
+        boundaries = find_boundaries(labels[i], mode = 'inner') > 0
+
+        semantic_seg = np.zeros_like(labels[i])
+        semantic_seg[labels[i]>1] = 2
+        semantic_seg[boundaries] = 3
+        semantic_seg[labels[i]==1] =1
+
+        semantic_seg_stack[i] = semantic_seg
+
+    return semantic_seg_stack
